02e785cbd7cc7ffbe829ce61b08cc95c000ddc13
ManojYadav2005 <160408265+ManojYadav2005@users.noreply.github.com>
2025-06-10 03:34:22 +0000

    comparison

--- basics/02conversionoperation.js
+++ basics/02conversionoperation.js
@@ -1,12 +1,12 @@
 
 let score=45
-console.log(typeof score);  // 1
-console.log(typeof (score));  // 2
+// console.log(typeof score);  // 1
+// console.log(typeof (score));  // 2
 
 let sco="45abc"
 let valnum=Number(sco)
-console.log(typeof valnum);  //3
-console.log(valnum);      // 4
+// console.log(typeof valnum);  //3
+// console.log(valnum);      // 4
 
 // "45"=>45
 // 45abc =>NaN
@@ -15,9 +15,40 @@ console.log(valnum);      // 4
 
 let isLoggedIn=""
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);    // 5
+// console.log(booleanIsLoggedIn);    // 5
 
 let s=56
 let sn=String(s)
-console.log(sn);         // 6
-console.log(typeof sn);   // 7
+// console.log(sn);         // 6
+// console.log(typeof sn);   // 7
+
+
+// *********************** OPERATION **************************
+
+// 1
+
+// let value=8;
+// let negval=-value
+// console.log(negval)
+// console.log(2+2);
+// console.log(2-2);
+
+// 2
+
+// let s1="Hello"
+// let s2="World"
+// console.log(s1+s2);
+
+// 3
+console.log("1" +2);
+console.log(2+4+5+"2");
+console.log("2"+4+5+2);
+console.log((3+4)*5%3);
+console.log(+true);
+console.log(true);
+
+// 4
+
+let gmcnt=100;
+gmcnt++;
+console.log(gmcnt);
