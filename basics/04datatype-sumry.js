c7f83c45a98b4bb47fc2e6a07eceff833e01a771
ManojYadav2005 <160408265+ManojYadav2005@users.noreply.github.com>
2025-06-10 03:54:56 +0000

    datatype-summary

---
+++ basics/04datatype-sumry.js
@@ -0,0 +1,46 @@
+// Primitive Data type
+
+// 7 type: String,NUmber,Boolean,Null, undefined,Symbol,BigInt
+
+// JavaScript is a dynamically typed language.  In other words, you do not need to declare the type of a variable when you create it.
+
+
+// Reference (Non Primitive)
+
+// Array,Obects,Functions
+
+const  heros = ["Shaktiman" , "Naagraj" , "Doga"];
+let myObj = {
+name: "Hitesh",
+age:22,
+
+}
+const myfuncn=function(){
+concole.log("Hello World");
+}
+
+const id= Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+console.log(typeof anotherId);
+
+
+
+
+// 1) Primitive DataType : -
+
+// i) Number => number
+// ii) String => string
+// iii) Symbol=symbol
+// iv) Null=> null
+// v) Boolean => boolean
+// vi) undefine=> undifine
+// vii) BigInt => bigint
+
+// 2 ) Non Primitive DataType :-
+
+
+// i)Array  =>  object
+// ii)  Object  => object 
+// iii) function =>  function
